d8ec51aa826ee46fe9eb3748e22bffc273b88230
Antonio1718 <antonio.jacques@servicepostal.com>
2019-05-17 18:23:47 +0200

    calculatrice

---
+++ src/component/calculate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Calculate extends Component {
+
+	render() {
+        return (
+            <div>
+            	<p>Hello React!!</p>
+            </div>
+        )
+    }
+}
+
+export default Calculate;
